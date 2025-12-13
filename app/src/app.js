b3e4dbfe7e38bf3745b7c2978c0159a68bb72f3c
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-12-13 12:47:44 -0500

    minor updates

--- app/src/app.js
+++ app/src/app.js
@@ -3,7 +3,9 @@ import { signIn, signOut, getUser } from "./auth.js";
 import { initUI } from "./ui/initUI.js";
 import { loadFragments } from "./ui/loadFragments.js";
 import { setupSubmitHandler } from "./ui/submitHandler.js";
-import { toggleForm } from "./ui/toggleform.js";
+import { toggleForm } from "./ui/toggleForm.js";
+import { updateUploadArea } from "./ui/toggleForm.js";
+
 
 async function init() {
  const ui = initUI();
@@ -12,12 +14,17 @@ async function init() {
 
   const user = await getUser();
   if (!user) return;
-
+  else ui.fragmentsSection.hidden = false
   ui.userSection.hidden = false;
   ui.loginBtn.disabled = true;
+
   ui.userSection.querySelector(".username").innerText = user.username;
 
   ui.addFragmentBtn.onclick = () => toggleForm(ui);
+  ui.typeSelect.addEventListener("change", () => {
+  updateUploadArea(ui.typeSelect.value, ui.container);
+});
+
   setupSubmitHandler(user, ui);
   await loadFragments(user, ui);
 }
