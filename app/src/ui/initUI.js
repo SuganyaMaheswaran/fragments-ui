531a0cd5db48231847f39ede24aa4cf0de890b75
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-12-14 11:51:16 -0500

    add additional dom elements

--- app/src/ui/initUI.js
+++ app/src/ui/initUI.js
@@ -1,4 +1,5 @@
-// initUI job is to collect all the key DOM elements by id, the app will interact with and return them as an object.
+// initUI job is to collect all the key DOM elements by id, 
+// the app will interact with and return them as an object.
 export function initUI(){
     return {
 
@@ -7,8 +8,11 @@ export function initUI(){
         logoutBtn: document.querySelector('#logout'),
         addFragmentBtn: document.querySelector('#addFragment'),
         fragmentForm: document.querySelector('#fragmentForm'),
-        fragmentsSection: document.querySelector('#fragmentsSection'),
+        fragmentsSection: document.querySelector('#fragmentsTableSection'),
         fragmentsTableBody:document.querySelector('#fragmentsTable tbody'),
-        typeSelect: document.getElementById("type")
+        typeSelect: document.getElementById("type"),
+        container: document.getElementById("containerUploadContent"),
+        fragmentAlertSection:document.getElementById(".fragmentAlertSection")
+        
     };
-}
+} 
